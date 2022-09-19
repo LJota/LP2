1671a7a9fb52ad2fc5cfc05fb2c190de9b0ad6d4
Lucas Rossi <99259327+LucasRossi3@users.noreply.github.com>
2022-09-19 19:28:58 -0300

    Add files via upload

---
+++ Atividade2/Atividade2/Atividade2/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace Atividade2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.lblNumero3 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(86, 397);
+            this.btnSomar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(194, 103);
+            this.btnSomar.TabIndex = 0;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.Location = new System.Drawing.Point(316, 397);
+            this.btnSubtrair.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(194, 103);
+            this.btnSubtrair.TabIndex = 1;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplicar.Location = new System.Drawing.Point(557, 397);
+            this.btnMultiplicar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(194, 103);
+            this.btnMultiplicar.TabIndex = 2;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(802, 397);
+            this.btnDividir.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(194, 103);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(798, 197);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(257, 133);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(798, 32);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(257, 133);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero1.Location = new System.Drawing.Point(101, 108);
+            this.lblNumero1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(94, 24);
+            this.lblNumero1.TabIndex = 6;
+            this.lblNumero1.Text = "Numero 1";
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero2.Location = new System.Drawing.Point(101, 205);
+            this.lblNumero2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(94, 24);
+            this.lblNumero2.TabIndex = 7;
+            this.lblNumero2.Text = "Numero 2";
+            // 
+            // lblNumero3
+            // 
+            this.lblNumero3.AutoSize = true;
+            this.lblNumero3.Location = new System.Drawing.Point(101, 301);
+            this.lblNumero3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblNumero3.Name = "lblNumero3";
+            this.lblNumero3.Size = new System.Drawing.Size(94, 24);
+            this.lblNumero3.TabIndex = 8;
+            this.lblNumero3.Text = "Resultado";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(272, 108);
+            this.txtNumero1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(275, 29);
+            this.txtNumero1.TabIndex = 9;
+            this.txtNumero1.Validated += new System.EventHandler(this.txtNumero1_Validated);
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(272, 205);
+            this.txtNumero2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(275, 29);
+            this.txtNumero2.TabIndex = 10;
+            this.txtNumero2.Validated += new System.EventHandler(this.txtNumero2_Validated_1);
+            // 
+            // txtNumero3
+            // 
+            this.txtNumero3.Enabled = false;
+            this.txtNumero3.Location = new System.Drawing.Point(272, 301);
+            this.txtNumero3.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtNumero3.Name = "txtNumero3";
+            this.txtNumero3.ReadOnly = true;
+            this.txtNumero3.Size = new System.Drawing.Size(275, 29);
+            this.txtNumero3.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 544);
+            this.Controls.Add(this.txtNumero3);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblNumero3);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.Label lblNumero3;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero3;
+    }
+}
+
---
+++ Atividade2/Atividade2/Atividade2/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade2
+{
+    public partial class Form1 : Form
+    {
+        Double numero1, numero2, resultado;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDividir_ClientSizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumero2.Text, out numero2) &&
+                double.TryParse(txtNumero1.Text, out numero1))
+            {
+                resultado = numero1 + numero2;
+                txtNumero3.Text = resultado.ToString();
+            }
+            else
+                MessageBox.Show("Números inválidos!");
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumero2.Text, out numero2) &&
+                double.TryParse(txtNumero1.Text, out numero1))
+            {
+                resultado = numero1 - numero2;
+                txtNumero3.Text = resultado.ToString();
+            }
+            else
+                MessageBox.Show("Números inválidos!");
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumero2.Text, out numero2) &&
+                double.TryParse(txtNumero1.Text, out numero1))
+            {
+                resultado = numero1 * numero2;
+                txtNumero3.Text = resultado.ToString();
+            }
+            else
+                MessageBox.Show("Números inválidos!");
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            txtNumero3.Text = "";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtNumero2.Text, out numero2) &&
+                double.TryParse(txtNumero1.Text, out numero1))
+            {
+                if (numero2 == 0)
+                    MessageBox.Show("Não pode dividir por zero!");
+                else
+                {
+                    resultado = numero1 / numero2;
+                    txtNumero3.Text = resultado.ToString();
+                }
+            }
+            else
+                MessageBox.Show("Números inválidos!");
+        }
+
+        private void txtNumero1_Validated(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(txtNumero1.Text, out numero1))
+            {
+                MessageBox.Show("Número inválido");
+            }
+        }
+
+        private void txtNumero2_Validated_1(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(txtNumero2.Text, out numero2))
+            {
+                MessageBox.Show("Número inválido");
+            }
+        }
+
+        
+
+
+    }
+}
