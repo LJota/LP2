6c02d74e3173ce49af5b8f93de0d4c844f0b26a1
Lucas Rossi <99259327+LucasRossi3@users.noreply.github.com>
2022-09-19 19:52:23 -0300

    Add files via upload

---
+++ Prova/PVenda/PVenda/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PVenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbxPlanilha = new System.Windows.Forms.ListBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstbxPlanilha
+            // 
+            this.lstbxPlanilha.FormattingEnabled = true;
+            this.lstbxPlanilha.ItemHeight = 16;
+            this.lstbxPlanilha.Location = new System.Drawing.Point(580, 12);
+            this.lstbxPlanilha.Name = "lstbxPlanilha";
+            this.lstbxPlanilha.Size = new System.Drawing.Size(668, 532);
+            this.lstbxPlanilha.TabIndex = 0;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(184, 104);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(208, 153);
+            this.btnVerificar.TabIndex = 1;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(184, 288);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(208, 130);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1387, 566);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lstbxPlanilha);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxPlanilha;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Prova/PVenda/PVenda/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PVenda
+{        //Prova feita por Matheus Gonsalves Favara e Lucas José Marcondes Rossi
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double[,] matrizVendas = new double[3, 4];
+            double totalVendasSem = 0;
+            double totalVendasMes = 0;
+            double totalVendasGeral = 0;
+            string dados;
+
+            int j;
+            int i;
+
+            for (i = 0; i < 3; i ++)
+            {
+                    for (j = 0; j < 4; j++)
+                {
+                    dados = Interaction.InputBox("Favor, digitar as vendas do mês " + (i + 1).ToString() + " da " + (j + 1).ToString() + "º semana: ", "Entrada de Dados");
+
+                    if (dados == "")
+                    {
+                        MessageBox.Show("Valor Incorreto!");
+                        j--;
+                    }
+                        else if (!(double.TryParse(dados, out matrizVendas[i, j])))
+                        {
+                             MessageBox.Show("Valor Incorreto!");
+                               j--;
+                        }
+                            else if (matrizVendas[i, j] < 0)
+                            {
+                                 MessageBox.Show("Valor Incorreto!");
+                                 j--;
+                            }
+                                else
+                                    {
+                                        lstbxPlanilha.Items.Add("Total do mês: " + (i + 1).ToString() + " Semana: " + (j + 1).ToString() + " " + matrizVendas[i, j].ToString("C2"));
+                                        totalVendasMes += matrizVendas[i, j];
+                                    }
+                    
+                }
+
+                    lstbxPlanilha.Items.Add(">> Total do mês: " + totalVendasMes.ToString("C2"));
+                    lstbxPlanilha.Items.Add("----------------------------------------------");
+                    totalVendasGeral += totalVendasMes;
+                    totalVendasMes = 0;
+            }
+
+            lstbxPlanilha.Items.Add(">> Total Geral: " + totalVendasGeral.ToString("C2"));
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
